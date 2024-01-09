d74f3e24c6bc520880ad1643798a606c8ee50014
Saida <saidabalsells@gmail.com>
2024-01-09 18:13:40 +0100

    first components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then( c => c.DashboardComponent)},
+  {path: 'user', loadComponent: () => import('./user/user.component').then( c => c.UserComponent)}
+];
